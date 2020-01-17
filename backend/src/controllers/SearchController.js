99b9bf024f4eb4df22b12b0c8def60dcbae627fd
gr8matheus <matheus93gs@hotmail.com>
2020-01-17 15:22:54 -0300

    Correções diversas

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -2,7 +2,7 @@ const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
-    async index(request, response){
+    async index(request, response) {
         const { latitude, longitude, techs } = request.query;
 
         const techsArray = parseStringAsArray(techs);
